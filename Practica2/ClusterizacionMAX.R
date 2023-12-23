9026694ab54d9d6d85182f1eb27b720e62e9e91b
Jrevenga06 <jorge.revenga@edu.uah.es>
2023-12-23 02:29:05 +0100

    .

--- Practica2/ClusterizacionMAX.R
+++ Practica2/ClusterizacionMAX.R
@@ -11,54 +11,40 @@ for (i in 1:n_puntos) {
   clusters[[i]] <- list(etiqueta = as.character(i), elementos = i)
 }
 
-# Función para calcular la distancia euclidiana entre dos clusters
-dist_clusters <- function(cluster1, cluster2) {
-  min_dist <- Inf
-  for (i in cluster1$elementos) {
-    for (j in cluster2$elementos) {
-      dist <- sqrt(sum((datos[i,] - datos[j,])^2))
-      if (dist < min_dist) {
-        min_dist <- dist
-      }
-    }
-  }
-  return(min_dist)
-}
-
-# Algoritmo jerárquico aglomerativo con MIN (single link)
+# Algoritmo jerárquico aglomerativo con MAX (complete link)
 iteraciones <- list()
 etiqueta <- 1
 while (length(clusters) > 1) {
-  # Inicializar la matriz de distancias entre clusters con Inf
-  distancias_clusters <- matrix(Inf, nrow = length(clusters), ncol = length(clusters))
+  # Inicializar la matriz de distancias entre clusters con -Inf
+  distancias_clusters <- matrix(-Inf, nrow = length(clusters), ncol = length(clusters))
   
   # Calcular la distancia entre cada par de clusters
   for (i in 1:(length(clusters) - 1)) {
     for (j in (i + 1):length(clusters)) {
-      distancias_clusters[i, j] <- dist_clusters(clusters[[i]], clusters[[j]])
+      distancias_clusters[i, j] <- dist_clusters_max(clusters[[i]], clusters[[j]])
     }
   }
   
-  # Encontrar el par de clusters más cercano
-  min_dist <- min(distancias_clusters)
-  min_index <- which(distancias_clusters == min_dist, arr.ind = TRUE)
+  # Encontrar el par de clusters más lejano
+  max_dist <- max(distancias_clusters, na.rm = TRUE)
+  max_index <- which(distancias_clusters == max_dist, arr.ind = TRUE)
   
-  # Unir los dos clusters más cercanos en uno nuevo
+  # Unir los dos clusters más lejanos en uno nuevo
   new_cluster <- list(etiqueta = paste("C", etiqueta, sep = ""),
-                      elementos = c(clusters[[min_index[1, 1]]]$elementos,
-                                    clusters[[min_index[1, 2]]]$elementos))
+                      elementos = c(clusters[[max_index[1, 1]]]$elementos,
+                                    clusters[[max_index[1, 2]]]$elementos))
   
   # Guardar la iteración actual
-  iteraciones[[length(iteraciones) + 1]] <- list(cluster1 = clusters[[min_index[1, 1]]],
-                                                 cluster2 = clusters[[min_index[1, 2]]],
+  iteraciones[[length(iteraciones) + 1]] <- list(cluster1 = clusters[[max_index[1, 1]]],
+                                                 cluster2 = clusters[[max_index[1, 2]]],
                                                  nuevo_cluster = new_cluster,
-                                                 distancia = min_dist)
+                                                 distancia = max_dist)
   
   # Incrementar la etiqueta para la próxima iteración
   etiqueta <- etiqueta + 1
   
   # Eliminar los clusters antiguos
-  clusters <- clusters[-c(min_index[1, 1], min_index[1, 2])]
+  clusters <- clusters[-c(max_index[1, 1], max_index[1, 2])]
   
   # Agregar el nuevo cluster
   clusters <- c(clusters, list(new_cluster))
@@ -77,77 +63,6 @@ hc <- hclust(dist(datos))
 dendro <- as.dendrogram(hc)
 dendro <- color_branches(dendro, k = length(iteraciones))
 plot(dendro)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
